eb622dd5776f1f6ffadcb7b80decf9c9803d4e8a
alkavan <igal.alkon@gmail.com>
2024-08-11 08:26:45 +0300

    fixing altitude read.

--- src/sensor.c
+++ src/sensor.c
@@ -11,8 +11,8 @@
 static void display_i2c_write(int opcode, const char * reg_name, uint8_t reg_addr, uint8_t value);
 static void float_to_string(float number, char *buffer, int decimal_places);
 
-static int32_t read_pressure(const uint8_t *sensor_buffer, bool raw);
-static float read_pressure_f(const uint8_t *sensor_buffer, bool raw);
+static int32_t read_pressure(const uint8_t *sensor_buffer);
+static float read_pressure_f(const uint8_t *sensor_buffer);
 
 static int32_t read_altitude(const uint8_t *sensor_buffer);
 static float read_altitude_f(const uint8_t *sensor_buffer);
@@ -42,7 +42,8 @@ bool activate_mpl3115a2_device(const struct device * i2c_dev, uint32_t i2c_cfg) 
 
 uint8_t operation_mode = MPL3115A2_CTRL_REG1_OS128
         /*| MPL3115A2_CTRL_REG1_RAW*/
-        | MPL3115A2_CTRL_REG1_BAR
+        /*| MPL3115A2_CTRL_REG1_BAR*/
+        | MPL3115A2_CTRL_REG1_ALT
         | MPL3115A2_CTRL_REG1_OST
         ;
 
@@ -85,7 +86,7 @@ bool do_mpl3115a2_cycle(
     sensor_error[0] = sensor_read_8bit(i2c_dev, MPL3115A2_REGISTER_STATUS, &sensor_buffer[0]);
 //    printk("[i2c] sensor status: %02x\n", sensor_buffer[0]);
 
-        /* Sensor data is ready */
+    /* Sensor data is ready */
     if(sensor_buffer[0] & MPL3115A2_STATUS_DATA_RDY_BITMASK) {
 //        printk("[i2c] data ready, reading sensors ... (sleep: %ums)\n", sleep_ms);
 
@@ -107,16 +108,19 @@ bool do_mpl3115a2_cycle(
                    sensor_error[0], sensor_error[1], sensor_error[2],
                    sensor_error[3], sensor_error[4], sensor_error[5]);
         } else {
-            float pressure_f = read_pressure_f(sensor_buffer, true);
+//            float pressure_f = read_pressure_f(sensor_buffer);
+            float altitude_f = read_altitude_f(sensor_buffer);
             float temperature_f = read_temperature_f(sensor_buffer);
 
             char buffer1[32] = {'\0'};
             char buffer2[32] = {'\0'};
 
-            float_to_string(pressure_f, buffer1, 3);
+            float_to_string(altitude_f, buffer1, 3);
+//            float_to_string(pressure_f, buffer1, 3);
             float_to_string(temperature_f, buffer2, 3);
 
-            printf("[i2c] pressure: ( %s hPa ) temperature: ( %s °C )\n", buffer1, buffer2);
+//            printf("[i2c] pressure: ( %s hPa ) temperature: ( %s °C )\n", buffer1, buffer2);
+            printf("[i2c] height: ( %s m ) temperature: ( %s °C )\n", buffer1, buffer2);
         }
 
         k_sleep(Z_TIMEOUT_MS(sleep_ms));
@@ -174,32 +178,24 @@ static void display_i2c_write(const int opcode, const char *reg_name, const uint
     }
 }
 
-static int32_t read_pressure(const uint8_t *sensor_buffer, bool raw)
+static int32_t read_pressure(const uint8_t *sensor_buffer)
 {
     // Combine the MSB, CSB, and LSB into a 20-bit value
-    uint32_t pressure_raw = ((uint32_t)sensor_buffer[1] << 16) |
-                            ((uint32_t)sensor_buffer[2] << 8)  |
-                            ((uint32_t)sensor_buffer[3]);
+    int32_t pressure = ((uint32_t)sensor_buffer[1] << 16) |
+                       ((uint32_t)sensor_buffer[2] << 8)  |
+                       ((uint32_t)sensor_buffer[3]);
 
-    int32_t pressure_pascals;
-
-    if(raw) {
-        pressure_pascals = pressure_raw;
-    } else {
-        pressure_pascals = pressure_raw >> 6;  // Shift right by 6 to remove fractional bits
-    }
-
-    return pressure_pascals;
+    return pressure;
 }
 
-static float read_pressure_f(const uint8_t *sensor_buffer, bool raw) {
-    return (float)read_pressure(sensor_buffer, raw) / (float)6400.0;
+static float read_pressure_f(const uint8_t *sensor_buffer) {
+    return (float)read_pressure(sensor_buffer) / (float)6400.0;
 }
 
 static int32_t read_altitude(const uint8_t *sensor_buffer) {
-    int32_t alt = ((uint32_t)sensor_buffer[0] << 24)
-                   | ((uint32_t)sensor_buffer[1] << 16)
-                   | ((uint32_t)sensor_buffer[2] << 8);
+    int32_t alt = ((uint32_t)sensor_buffer[1] << 24) |
+                  ((uint32_t)sensor_buffer[2] << 16) |
+                  ((uint32_t)sensor_buffer[3] << 8);
 
     return alt;
 }
@@ -211,7 +207,8 @@ static float read_altitude_f(const uint8_t *sensor_buffer)
 
 static uint16_t read_temperature(const uint8_t *sensor_buffer)
 {
-    return ( (uint16_t)sensor_buffer[4] << 8 ) | ( (uint16_t)sensor_buffer[5] );
+    return ((uint16_t)sensor_buffer[4] << 8) |
+           ((uint16_t)sensor_buffer[5]);
 }
 
 static float read_temperature_f(const uint8_t *sensor_buffer) {
@@ -225,16 +222,14 @@ static void float_to_string(float number, char *buffer, int decimal_places)
 
     // Calculate the fractional part
     int multiplier = 1;
-    for (int i = 0; i < decimal_places; i++)
-    {
+    for (int i = 0; i < decimal_places; i++) {
         multiplier *= 10;
     }
 
     int fractional_part = (int)((number - integer_part) * multiplier);
 
     // Handle negative numbers
-    if (fractional_part < 0)
-    {
+    if (fractional_part < 0) {
         fractional_part = -fractional_part;
     }
 
